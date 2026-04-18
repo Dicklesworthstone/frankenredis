6265b5faef5561580a8e1c8007e5661f58af3a1f
Dicklesworthstone <jeff141421@gmail.com>
2026-04-18 19:40:53 +0000

    test(fuzz): update fuzz_sentinel_parsers to include parse_info_response [frankenredis-z9oj]

--- fuzz/fuzz_targets/fuzz_sentinel_parsers.rs
+++ fuzz/fuzz_targets/fuzz_sentinel_parsers.rs
@@ -1,6 +1,7 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use fr_sentinel::discovery::{HelloMessage, parse_replica_info_from_master};
+use fr_sentinel::health::parse_info_response;
 
 fuzz_target!(|data: &[u8]| {
     if data.len() > 1_000_000 {
@@ -10,5 +11,6 @@ fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let _ = HelloMessage::parse(s);
         let _ = parse_replica_info_from_master(s);
+        let _ = parse_info_response(s);
     }
 });
