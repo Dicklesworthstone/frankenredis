b1b0c82dc31d5b7b37ec00b641f1e2a6fc5c71d0
Dicklesworthstone <jeff141421@gmail.com>
2026-04-18 19:16:36 +0000

    test(fuzz): add fuzz target for AofManifest parser [frankenredis-hanq]

---
+++ fuzz/fuzz_targets/fuzz_aof_manifest_parser.rs
@@ -0,0 +1,12 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use fr_persist::parse_aof_manifest;
+
+fuzz_target!(|data: &[u8]| {
+    if data.len() > 1_000_000 {
+        return;
+    }
+    if let Ok(s) = std::str::from_utf8(data) {
+        let _ = parse_aof_manifest(s);
+    }
+});
