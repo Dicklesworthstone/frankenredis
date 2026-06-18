ce59a7fdcfda31ca071434066544e9967a71be45
Dicklesworthstone <jeff141421@gmail.com>
2026-06-17 22:41:43 -0400

    test(scripts): cover COPY DB key extraction (frankenredis-wxeea)

--- scripts/getkeys_flags_differ.py
+++ scripts/getkeys_flags_differ.py
@@ -75,6 +75,7 @@ PROBES = [
     ["set", "k", "v"], ["get", "k"], ["getex", "k"], ["getdel", "k"],
     ["mset", "k1", "v1", "k2", "v2"], ["mget", "k1", "k2", "k3"],
     ["expire", "k", "100"], ["copy", "src", "dst"], ["copy", "src", "dst", "REPLACE"],
+    ["copy", "src", "dst", "DB", "1"], ["copy", "src", "dst", "DB", "1", "REPLACE"],
     ["rename", "a", "b"], ["smove", "s", "d", "m"],
     ["lmpop", "2", "k1", "k2", "LEFT"], ["zmpop", "2", "z1", "z2", "MIN"],
     ["blmpop", "0", "2", "k1", "k2", "LEFT"], ["bzmpop", "0", "2", "z1", "z2", "MIN"],
